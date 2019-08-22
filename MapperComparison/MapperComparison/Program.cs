3ebcb07368a0cb29e4873b889d51704755ca8009
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2019-08-21 20:49:26 -0400

    Add Mapster project

---
+++ MapperComparison/MapperComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MapperComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
